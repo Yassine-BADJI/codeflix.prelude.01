feb1416e09bb244304c44c39ac69752a2ef6d309
Yassine-BADJI <yassine@badji.fr>
2019-10-11 09:56:48 +0200

    work from home

---
+++ ex06.js
@@ -0,0 +1,23 @@
+module.exports =
+	function kebabcase(str = "") {
+	let finish
+
+	if(str.match(/-/))
+	{
+		finish = str.replace(/(^-|-$)/g, "")
+	}
+	else if (str.match(/([A-Z])/g))
+	{
+		finish = str.replace(/([A-Z])/g, " $1")
+		finish = finish.replace(/ /, "")
+		finish = finish.replace(/ /g, "-")
+	}
+	else
+	{
+		finish = str.replace(/ /g, "-")
+	}
+	finish = finish.toLowerCase()
+
+	return finish
+}
+
